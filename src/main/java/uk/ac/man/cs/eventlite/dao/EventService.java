079a60f32ed3ea3ac2c10f2c242151285cee1478
George Chiriluta <george.chiriluta@student.manchester.ac.uk>
2019-02-26 13:09:47 +0000

    Fix Event Service conflict

--- src/main/java/uk/ac/man/cs/eventlite/dao/EventService.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/EventService.java
@@ -10,11 +10,9 @@ public interface EventService {
 	
 	public Event findOne(long id);
 	
-<<<<<<< Upstream, based on branch 'MVP_dev' of ssh://gitlab@gitlab.cs.man.ac.uk:22222/comp23412_2018/eventlite_G06.git
 	public Event save(Event event);
 
 	public void delete(long id);
-=======
+
 	public Iterable<Event> findAllAfterToday();
->>>>>>> df6f006 Allow updating only of the past events
 }
