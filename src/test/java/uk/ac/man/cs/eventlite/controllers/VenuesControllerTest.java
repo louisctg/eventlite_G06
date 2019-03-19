18ec1108a916c64e81cb57da8e4e828a30a3d12c
Melvyn Tie <melvyn.tie@student.manchester.ac.uk>
2019-03-19 16:08:24 +0000

    Add separate venue page controller test

---
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
@@ -0,0 +1,78 @@
+
+
+package uk.ac.man.cs.eventlite.controllers;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Collections;
+
+import javax.servlet.Filter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import uk.ac.man.cs.eventlite.EventLite;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EventLite.class)
+@AutoConfigureMockMvc
+@DirtiesContext
+@ActiveProfiles("test")
+public class VenuesControllerTest {
+
+	private MockMvc mvc;
+
+	@Autowired
+	private Filter springSecurityFilterChain;
+
+	@Mock
+	private Venue venue;
+
+	@Mock
+	private VenueService venueService;
+
+	@InjectMocks
+	private VenuesController venuesController;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		mvc = MockMvcBuilders.standaloneSetup(venuesController).apply(springSecurity(springSecurityFilterChain))
+				.build();
+	}
+
+	@Test
+	public void getIndexVenue() throws Exception {
+
+		when(venueService.findOne(0)).thenReturn(venue);
+
+		mvc.perform(get("/venues/0").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+		.andExpect(view().name("venues/venue")).andExpect(handler().methodName("getVenue"));
+
+		verify(venueService).findOne(1);
+		verifyZeroInteractions(venue);
+	}
+}
