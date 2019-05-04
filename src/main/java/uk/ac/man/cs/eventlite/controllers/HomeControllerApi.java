c4d40462e08db57d7a414902a3b7eaadc118bfcf
z26937hc <haorui.chen@student.manchester.ac.uk>
2019-05-04 16:10:20 +0100

    Venue owner-checking next 3 events in rest implemented.

---
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/HomeControllerApi.java
@@ -0,0 +1,34 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@RestController
+@RequestMapping(value="/api", produces = { MediaType.APPLICATION_JSON_VALUE,  MediaTypes.HAL_JSON_VALUE})
+public class HomeControllerApi {
+	
+	@RequestMapping(method= RequestMethod.GET)
+	public Resource<Home> getAllHomeLinks(){
+		Link profileLink = linkTo(methodOn(HomeControllerApi.class).getAllHomeLinks()).slash("profile").withRel("profile");
+		Link eventsLink = linkTo(EventsControllerApi.class).withRel("events");
+		Link venuesLink = linkTo(EventsControllerApi.class).withRel("venues");
+		
+		return new Resource<Home>(new Home(), venuesLink, eventsLink, profileLink);
+	}
+	
+	@JsonIgnoreProperties
+	public class Home{
+		
+	}
+	
+}
