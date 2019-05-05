fca5f4d5909086ceb046178c5a126ce15ce4172d
George Chiriluta <george.chiriluta@student.manchester.ac.uk>
2019-05-05 19:53:14 +0100

    Modify getEvent in API and test it

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApi.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApi.java
@@ -43,8 +43,9 @@ public class EventsControllerApi {
 	
 	private Resource<Event> eventToResource(Event event) {
 		Link selfLink = linkTo(EventsControllerApi.class).slash(event.getId()).withSelfRel();
+		Link eventLink = linkTo(EventsControllerApi.class).slash(event.getId()).withRel("event");
 		Link venueLink = linkTo(EventsControllerApi.class).slash(event.getId()).slash("venue").withRel("venue");
-		return new Resource<Event>(event, selfLink,venueLink);
+		return new Resource<Event>(event, selfLink, eventLink, venueLink);
 	}
 
 	public Resources<Resource<Event>> eventToResource(Iterable<Event> events) {
--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApiTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerApiTest.java
@@ -14,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static uk.ac.man.cs.eventlite.testutil.MessageConverterUtil.getMessageConverters;
 
 import java.util.Collections;
+import java.util.Date;
 
 import javax.servlet.Filter;
 
@@ -80,13 +81,39 @@ public class EventsControllerApiTest {
 		when(eventService.findAll()).thenReturn(Collections.<Event> singletonList(e));
 
 		mvc.perform(get("/api/events").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(handler().methodName("getAllEvents")).andExpect(jsonPath("$.length()", equalTo(2)))
+				.andExpect(handler().methodName("getAllEvents"))
+				.andExpect(jsonPath("$.length()", equalTo(2)))
 				.andExpect(jsonPath("$._links.self.href", endsWith("/api/events")))
 				.andExpect(jsonPath("$._embedded.events.length()", equalTo(1)))
 				.andExpect(jsonPath("$._embedded.events[0]._links.venue.href", not(empty())))
 				.andExpect(jsonPath("$._embedded.events[0]._links.venue.href", endsWith("events/0/venue")));
 
 		verify(eventService).findAll();
+	}
+	
+	@Test
+	public void getEventTest() throws Exception
+	{
+		Event e = new Event();
+		e.setId(0);
+		e.setName("test");
+		e.setDate(new Date());
+		e.setTime(new Date());
+		when(eventService.findOne(0)).thenReturn(e);
+		
+		mvc.perform(get("/api/events/0").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(handler().methodName("getEvent"))
+				.andExpect(jsonPath("$.length()", equalTo(6)))
+				.andExpect(jsonPath("$.date", not(empty())))
+				.andExpect(jsonPath("$.time", not(empty())))
+				.andExpect(jsonPath("$.name", not(empty())))
+				.andExpect(jsonPath("$.organiser", not(empty())))
+				.andExpect(jsonPath("$.description", not(empty())))
+				.andExpect(jsonPath("$._links.self.href", endsWith("/api/events/0")))
+				.andExpect(jsonPath("$._links.event.href", endsWith("/api/events/0")))
+				.andExpect(jsonPath("$._links.venue.href", endsWith("/api/events/0/venue")));
+
+		verify(eventService).findOne(0);
 	}
 
 }
